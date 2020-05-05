8e6659f050dfbefc532eb62e9611f179ef5ee844
imshaz <imshazbangash@gmail.com>
2020-05-05 11:22:56 -0400

    Output management

--- src/index.js
+++ src/index.js
@@ -1,22 +1,24 @@
 import _ from "lodash";
 import "./style.css";
-import Laptop from "./assets/images/laptop.jpeg";
+
+import printMe from './print.js';
 
 // import xml data - loader is already added in webpack
-
 import xmlData from "./data/note.xml";
-import jsonData from "./data/jsonData.json";
+import jsonData from "./data/jsonData.json"; //json is added by default. no external loader needed
 
 function component() {
   console.log('xmlData',xmlData.note);
   console.log('jsonData',jsonData);
-
+const btn = document.createElement('button');
+btn.innerHTML = 'Click me and check the console!';
+ btn.onclick = printMe;
   const element = document.createElement("div");
-  const img = document.createElement("img");
-  img.src = Laptop;
+
   element.innerHTML = _.join(["Hello", "webpack"], "-");
   element.classList.add("hello");
-  element.appendChild(img);
+ element.appendChild(btn);
+ 
   return element;
 }
 
