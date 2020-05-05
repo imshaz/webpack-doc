079e829652c3343fcc7d08dca01ad5cef8451609
imshaz <imshazbangash@gmail.com>
2020-05-05 09:48:09 -0400

    Loading data

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import _ from "lodash";
 import "./style.css";
 import Laptop from "./assets/images/laptop.jpeg";
+
+// import xml data - loader is already added in webpack
+
+import xmlData from "./data/note.xml";
+import jsonData from "./data/jsonData.json";
+
 function component() {
+  console.log('xmlData',xmlData.note);
+  console.log('jsonData',jsonData);
+
   const element = document.createElement("div");
   const img = document.createElement("img");
   img.src = Laptop;
--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,14 @@ module.exports = {
         test: /\.(woff|woff2|eot|ttf|otf)$/,
         use: ["file-loader"],
       },
+      {
+        test: /\.(csv|tsv)$/,
+        use: ["csv-loader"],
+      },
+      {
+        test: /\.xml$/,
+        use: ["xml-loader"],
+      },
     ],
   },
 };
