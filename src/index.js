5ba3ba7f71ea5faf2911b49fcb686a6cc98b927f
imshaz <imshazbangash@gmail.com>
2020-05-05 08:58:49 -0400

    Loading images

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import _ from "lodash";
+import "./style.css";
+import Laptop from "./assets/images/laptop.jpeg";
 function component() {
   const element = document.createElement("div");
-
+  const img = document.createElement("img");
+  img.src = Laptop;
   element.innerHTML = _.join(["Hello", "webpack"], "-");
-
+  element.classList.add("hello");
+  element.appendChild(img);
   return element;
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,21 @@
-const path = require('path');
+const path = require("path");
 
-module.exports ={
-    entry:'./src/index.js',
-    output:{
-        filename:'main.js',
-        path:path.resolve(__dirname, 'dist')
-    }
-}
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpeg|jpg|gif)$/,
+        use: ["file-loader"],
+      },
+    ],
+  },
+};
